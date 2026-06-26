66883e2beb3ad5bef56bed517ff380dd07d153b6
Hirmas <hirmasn9@gmail.com>
2026-06-26 21:35:27 +0700

    feat: setup express server and mongodb connection

---
+++ backend/src/server.ts
@@ -0,0 +1,27 @@
+import express, { type Application, type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectionDB from './config/db.js';
+
+// Load environment .env
+dotenv.config();
+
+// Connect to database
+connectionDB();
+
+const app: Application = express();
+
+// Middleware
+app.use(cors()); // Hit API from different domain 
+app.use(express.json()); // Parse JSON request body
+
+// Routes
+app.get('/api/health', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'API is running smoothly!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen (PORT, () => {
+    console.log(`[🚀] Server is running on port ${PORT}`);
+});
